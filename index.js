27a2cd68168df6d523bf7e316ef8963bd3c312f9
Arijit15091999 <arijitkarmakar056@gmail.com>
2024-06-30 16:08:44 +0530

    added mobile touch

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
-const body = document.querySelector("body");
-
+const body = document.getElementById("animation-body");
 
 // for pc
-// body.addEventListener("mousemove", 
-//     function(event) {
-//         let span = document.createElement("span");
-//         let xPos = event.offsetX;
-//         let yPos = event.offsetY;
-//         let randomSize = Math.random() * 100 + 1;
-//         span.style.left = xPos + "px";
-//         span.style.top = yPos + "px";
-//         span.style.height = randomSize + "px";
-//         span.style.width = randomSize + "px";
-//         body.appendChild(span);
-//         setTimeout(
-//             function(){
-//                 span.remove();
-//             }
-//             ,3000
-//         );
-//     }
-// )
+body.addEventListener("mousemove", function (event) {
+  let span = document.createElement("span");
+  let xPos = event.offsetX;
+  let yPos = event.offsetY;
+  let randomSize = Math.random() * 100 + 1;
+  span.style.left = xPos + "px";
+  span.style.top = yPos + "px";
+  span.style.height = randomSize + "px";
+  span.style.width = randomSize + "px";
+  body.appendChild(span);
+  setTimeout(function () {
+    span.remove();
+  }, 3000);
+});
 
+function eventHandler(event) {
+  // console.log(event)
+  let span = document.createElement("span");
+  let xPos = event.touches[0].screenX;
+  let yPos = event.touches[0].screenY;
+  let randomSize = Math.random() * 100 + 1;
+  span.style.left = xPos + "px";
+  span.style.top = yPos + "px";
+  span.style.height = randomSize + "px";
+  span.style.width = randomSize + "px";
+  body.appendChild(span);
+  setTimeout(function () {
+    span.remove();
+  }, 3000);
+
+  console.log(event.touches);
+}
 
 //for mobile
-body.addEventListener("touchmove", 
-    function(event) {
-        let span = document.createElement("span");
-        let xPos = event.offsetX;
-        let yPos = event.offsetY;
-        let randomSize = Math.random() * 100 + 1;
-        span.style.left = xPos + "px";
-        span.style.top = yPos + "px";
-        span.style.height = randomSize + "px";
-        span.style.width = randomSize + "px";
-        body.appendChild(span);
-        setTimeout(
-            function(){
-                span.remove();
-            }
-            ,3000
-        );
-    }
-)
+body.addEventListener("touchstart", eventHandler);
+body.addEventListener("touchmove", eventHandler);
