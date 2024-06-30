12935169e26465dae2d16f98c3f70a61e94c5633
Arijit15091999 <155151121+Arijit15091999@users.noreply.github.com>
2024-06-30 15:07:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const body = document.querySelector("body");
 
 
 // for pc
-body.addEventListener("mousemove", 
-    function(event) {
-        let span = document.createElement("span");
-        let xPos = event.offsetX;
-        let yPos = event.offsetY;
-        let randomSize = Math.random() * 100 + 1;
-        span.style.left = xPos + "px";
-        span.style.top = yPos + "px";
-        span.style.height = randomSize + "px";
-        span.style.width = randomSize + "px";
-        body.appendChild(span);
-        setTimeout(
-            function(){
-                span.remove();
-            }
-            ,3000
-        );
-    }
-)
+// body.addEventListener("mousemove", 
+//     function(event) {
+//         let span = document.createElement("span");
+//         let xPos = event.offsetX;
+//         let yPos = event.offsetY;
+//         let randomSize = Math.random() * 100 + 1;
+//         span.style.left = xPos + "px";
+//         span.style.top = yPos + "px";
+//         span.style.height = randomSize + "px";
+//         span.style.width = randomSize + "px";
+//         body.appendChild(span);
+//         setTimeout(
+//             function(){
+//                 span.remove();
+//             }
+//             ,3000
+//         );
+//     }
+// )
 
 
 //for mobile
